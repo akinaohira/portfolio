c862ac3e2f04fc28c1c0724905192ddd7090895f
akina ohira <akina568325@gmail.com>
2021-02-26 14:31:32 +0900

    Completed pashirin page design

--- src/components/Pashirin.js
+++ src/components/Pashirin.js
@@ -4,21 +4,40 @@ import pashirinFlash from "../img/pashirin_flash.png"
 import "../css/App.css"
 import { ReactComponent as Github } from "../img/github.svg"
 import { ReactComponent as Youtube } from "../img/youtube.svg"
+import { ReactComponent as LeftArrow } from "../img/arrow-left.svg"
+
+import userOrder from "../img/user-order.png";
+import usePashirin from "../img/user_pashirin.png"
+import watingDeli from "../img/user-watingDelivery.png"
+import login from "../img/login.png"
+import done from "../img/pashirin-finish.png"
 
 function Pashirin() {
-  return <div className="frame_indivisual">
+  return <div className="frame_indivisual" id="pashirin_back">
+    <Link to="/" className="arrow_box"><LeftArrow className="arrow_icon" /></Link>
     <div>
-      <img src={pashirinFlash} alt="frame"></img>
-      <div>
+      <div className="img_box">
+        <img src={pashirinFlash} alt="frame"></img>
+      </div>
+      <div className="exaplin_box">
         <h1>Pashirin</h1>
         <p>iOS app that connects users who need help with something with people who can fulfill those    tasks, tracks deliveries by GPS, and enables payments using the Stripe API.
         </p>
-        <a href="https://youtu.be/gtWhv1qVqBQ?t=8"><Youtube /></a>
-        <a href="http://github.com/Pashirin/pashirin-frontend-new"><Github /></a>
+        <div>
+          <a href="https://youtu.be/gtWhv1qVqBQ?t=8"><Youtube className="icon" /></a>
+          <a href="http://github.com/Pashirin/pashirin-frontend-new"><Github className="icon" /></a>
+        </div>
+        <div>
+          <img alt="pashirin" src={usePashirin} className="app_detail_img" id="left_img"></img>
+          <img alt="pashirin" src={userOrder} className="app_detail_img"></img>
+          <img alt="pashirin" src={watingDeli} className="app_detail_img"></img>
+          <img alt="pashirin" src={login} className="app_detail_img"></img>
+          <img alt="pashirin" src={done} className="app_detail_img"></img>
+        </div>
       </div>
     </div>
-    <Link to="/">Back</Link>
-  </div>
+
+  </div >
 
 
 }
